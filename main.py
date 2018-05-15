8faf4a78860073a1d5d30f3908a1a8217baddf1c
Magdz <ahmed.magdy@ivasystem.fr>
2018-05-15 20:16:52 +0200

    Fix conflict

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from Lexical import Lexical
-<<<<<<< HEAD
 from Parser import Parser   
 
 lexical = Lexical('rules.txt')
@@ -10,10 +9,3 @@ parser = Parser('grammar.txt')
 tree = parser.analyze(tokens)
 for item in tree:
     print item
-=======
-from CFG import CFG
-# lexical = Lexical('rules.txt')
-# tokens = lexical.analize('code.txt')
-# print tokens
-CFG('grammar.txt')
->>>>>>> 613813030fa9470a48070fb5308fbb79e4bfeb42
